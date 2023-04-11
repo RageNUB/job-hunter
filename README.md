9fcc700b6f20b0a95bbcd9ed9e0d93e9ec73ffe1
RageNUB <jirimon39@gmail.com>
2023-04-12 00:43:24 +0600

    Readme file added

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # b7a9-career-hub-RageNUB
+# Job Hunter
+
+## Best Features
+
+- So many available jobs for everyone.
+- Full Time and Part time jobs.
+- Remote and Onsite jobs.
+- You can view every jobs full details along with salary range.
+- You can view your previously applied jobs anytime.
+- Here's a filter option that you can filter remote or onsite jobs.
+- here's a Statistic page where my all previous assignment marks are shown as a Radar Chart.
+- Here's a Blog page where you can find 4 questions about some topic and their answers.
+
+## 🔗 Live Site Link
+
+ - [Netlify Link](https://dashing-elf-5c6360.netlify.app/)
+ - [Surge Link](https://heavy-attention.surge.sh/)
--- src/component/JobCategory/JobCategory.jsx
+++ src/component/JobCategory/JobCategory.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import JobList from '../JobList/JobList';
 
 const JobCategory = () => {
-    const jobList = [
-        {id:1, image:"./assets/Icons/accounts.png", title:"Account & Finance", vacancy:"300 Jobs Available"},
-        {id:2, image:"./assets/Icons/business.png", title:"Creative Design", vacancy:"100+ Jobs Available"},
-        {id:3, image:"./assets/Icons/chip.png", title:"Marketing & Sales", vacancy:"150 Jobs Available"},
-        {id:4, image:"./assets/Icons/social-media.png", title:"Engineering Job", vacancy:"224 Jobs Available"}
-    ]
-    // console.log(jobList)
+    const [jobList, setJobList] = useState([])
+
+    useEffect(() => {
+        fetch("/jobCategory.json")
+        .then(res => res.json())
+        .then(data => setJobList(data))
+    },[])
+    
     return (
         <div className='mt-16'>
             <div className='text-center'>
